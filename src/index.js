4b84b32fd4ef49d96386ba380fe798bd8c25dc73
Pablo Marin <pabletegym@icloud.com>
2023-07-26 17:08:27 +0200

    Editing navbar and adding submenu

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+// Once the document is ready, add all the events
 $(document).ready(function(){
+    //Adding the event for the phone button
     $(".phone-btn").on("click",phonebtn_clicked)
+
+    //Adding the events for the submenus
+    $(".submenu-btn").each(function(){
+        id= $(this).attr("id")
+        console.log(id)
+        $(this).on("click",function(){
+            ShowSubmenu("submenu-"+id)
+        })
+    })
 })
 
-
-
+/* Triggered functions */
 function phonebtn_clicked(){
     $(".navbar").toggleClass("show")
     $(".phone-btn").toggleClass("close")
+}
+
+function ShowSubmenu(submenu_class){
+    $("."+submenu_class).toggleClass("show")
 }
