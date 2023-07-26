39430f397f7713babc5cbd79274474fa1fadd55a
Pablo Marin <pabletegym@icloud.com>
2023-07-26 19:35:31 +0200

    Hidding navbar and submenus when change screen width

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ var small_screen_limit=470
 
 // Once the document is ready, add all the events
 $(document).ready(function(){
+    //Setting initial window width vars
+    if($(window).width()<=small_screen_limit){
+        small_screen=true
+    }
     //Adding the event for the phone button
     $(".phone-btn").on("click",phonebtn_clicked)
 
@@ -24,4 +28,23 @@ function phonebtn_clicked(){
 
 function ShowSubmenu(submenu_class){
     $("."+submenu_class).toggleClass("show")
+}
+
+function WindowResize(){
+    console.log(small_screen)
+    if(($(window).width()<=small_screen_limit && small_screen==false) || ($(window).width()>small_screen_limit && small_screen==true)){
+        //Change to screen type
+        $(".navbar").removeClass("show")
+        $(".phone-btn").removeClass("close")
+        $(".submenu-btn").each(function(){
+            id= $(this).attr("id")
+            $(".submenu-"+id).removeClass("show")
+        })
+        if(small_screen){
+            small_screen=false
+        }
+        else{
+            small_screen=true
+        }
+    }
 }
